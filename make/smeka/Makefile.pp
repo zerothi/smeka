f608267ab007cca37a1345d490e50208e4814a1a
Nick Papior <nickpapior@gmail.com>
2016-04-26 13:10:35 +0200

    enh: Added OS settings and moved PP to different file
    The OS detection is now performed upon creation
    
    The PP should be created based on the OS and
    *possibly* by vendor.
    
    Signed-off-by: Nick Papior <nickpapior@gmail.com>

---
+++ make/smeka/Makefile.pp
@@ -0,0 +1,36 @@
+_SMEKA_pp = 1
+.PHONY: show
+
+# This smeka file creates the preprocessor
+# settings.
+
+# Default pre-processor commands
+# (probably we should do a check to ensure
+#  it works)
+CPP ?= cpp -E -P -C -nostdinc
+CPPFLAGS ?=
+ifeq ($(uname_S),Linux)
+endif
+ifeq ($(uname_S),Darwin)
+endif
+ifeq ($(uname_S),SunOS)
+endif
+ifeq ($(uname_S),FreeBSD)
+endif
+ifeq ($(uname_S),OpenBSD)
+endif
+ifeq ($(uname_S),NetBSD)
+endif
+ifeq ($(uname_S),Cygwin)
+endif
+
+.NOTPARALLEL: show
+show::
+	@echo "  CPP      = $(CPP)"
+	@echo "  CPPFLAGS = $(CPPFLAGS)"
+	@echo "  INCLUDES = $(INCLUDES)"
+
+
+# Local Variables:
+#  mode: makefile-gmake
+# End:
