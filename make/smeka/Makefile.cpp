2746590c890937e396537f9e0739585b6bd704ad
Nick Papior <nickpapior@gmail.com>
2016-07-26 08:53:46 +0200

    enh: the CPPFLAGS should not adhere to the CPP cmd
    The CPPFLAGS should only define pre-processor flags
    so that they may be passed through any compiler which does
    on the fly preprocessing.
    
    Signed-off-by: Nick Papior <nickpapior@gmail.com>

--- make/smeka/Makefile.cpp
+++ make/smeka/Makefile.cpp
@@ -6,7 +6,6 @@ _SMEKA_cpp = 1
 # Default pre-processor commands
 # (probably we should do a check to ensure
 #  it works)
-CPP ?= cpp
 # Sadly there are many variations of CPP
 #   -C is needed to disregard C-comments (the operator // is a comment!?!)
 #   -nostdinc is needed to not include standard includes (which are C comments)
@@ -15,7 +14,8 @@ CPP ?= cpp
 # clang-3.5 (MacOSx): -E -EP -xc
 # clang-3.7> (MacOSx): -E -P -x c
 # cpp(gnu): -E -P -x c -nostdinc
-CPPFLAGS ?= -E -P -x c
+CPP ?= cpp -E -P -x c
+CPPFLAGS ?=
 
 ifeq ($(uname_S),Linux)
 endif
